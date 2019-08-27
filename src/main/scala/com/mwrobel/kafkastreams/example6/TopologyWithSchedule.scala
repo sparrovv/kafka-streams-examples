b27868cc28ce4ef26ecfa02fb0840cc9e747bb8d
Michal Wrobel <michal.wrobel@simplybusiness.co.uk>
2019-08-27 10:31:42 +0100

    Add enrichments to the store

--- src/main/scala/com/mwrobel/kafkastreams/example6/TopologyWithSchedule.scala
+++ src/main/scala/com/mwrobel/kafkastreams/example6/TopologyWithSchedule.scala
@@ -21,6 +21,27 @@ object ContactRequestsStore {
   val valSerde = ContactRequest.serde
 
   type ContactRequests = KeyValueStore[String, ContactRequest]
+
+  object Enrichments {
+    implicit class ContactRequestScheduleStorageEnrichments(val contactRequestsStore: ContactRequests) {
+      def upsertContactRequest(contactRequest: ContactRequest) = {
+        contactRequestsStore.put(contactRequest.userId, contactRequest)
+      }
+
+      def foreach(f: ContactRequest => Unit): Unit = {
+        val iterator = contactRequestsStore.all
+
+        try {
+          while (iterator.hasNext) {
+            f(iterator.next.value)
+          }
+        } finally {
+          iterator.close()
+        }
+      }
+    }
+  }
+
 }
 
 object TopologyWithSchedule extends LazyLogging {
--- src/main/scala/com/mwrobel/kafkastreams/example6/transformers/ScheduleContactRequests.scala
+++ src/main/scala/com/mwrobel/kafkastreams/example6/transformers/ScheduleContactRequests.scala
@@ -3,17 +3,18 @@ package com.mwrobel.kafkastreams.example6.transformers
 import java.time
 import com.mwrobel.kafkastreams.example6.models.ContactRequest
 import com.mwrobel.kafkastreams.example6.{ContactRequestsStore}
-import com.mwrobel.kafkastreams.utils.CloseableResource
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.kstream.Transformer
 import org.apache.kafka.streams.processor.{ProcessorContext, PunctuationType, Punctuator}
 import org.joda.time.DateTime
 
-case class ScheduleContactRequests(scheduleInterval: Int = 10000, scheduledAtSetter: (ContactRequest) => ContactRequest)
+case class ScheduleContactRequests(scheduleInterval: Int = 10000, setScheduledAt: (ContactRequest) => ContactRequest)
     extends Transformer[String, ContactRequest, KeyValue[String, ContactRequest]]
     with LazyLogging {
   var contactRequestsStore: ContactRequestsStore.ContactRequests = _
+
+  import ContactRequestsStore.Enrichments._
 
   override def init(context: ProcessorContext): Unit = {
     contactRequestsStore =
@@ -25,16 +26,12 @@ case class ScheduleContactRequests(scheduleInterval: Int = 10000, scheduledAtSet
       (timestamp: Long) => {
         val now = new DateTime(timestamp)
 
-        CloseableResource(contactRequestsStore.all()) { record =>
-          while (record.hasNext) {
-            val contactRequest = record.next().value
+        contactRequestsStore.foreach { contactRequest =>
+          if (contactRequest.isReadyToSend(now)) {
+            val updatedContactRequest = contactRequest.copy(forwarded = true)
+            contactRequestsStore.upsertContactRequest(updatedContactRequest)
 
-            if (contactRequest.isReadyToSend(now)) {
-              val updatedContactRequest = contactRequest.copy(forwarded = true)
-              contactRequestsStore.put(updatedContactRequest.userId, updatedContactRequest)
-
-              context.forward(updatedContactRequest.userId, updatedContactRequest)
-            }
+            context.forward(updatedContactRequest.userId, updatedContactRequest)
           }
         }
       }
@@ -42,7 +39,7 @@ case class ScheduleContactRequests(scheduleInterval: Int = 10000, scheduledAtSet
   }
 
   override def transform(key: String, value: ContactRequest): KeyValue[String, ContactRequest] = {
-    contactRequestsStore.put(value.userId, scheduledAtSetter(value))
+    contactRequestsStore.upsertContactRequest(setScheduledAt(value))
 
     null
   }
